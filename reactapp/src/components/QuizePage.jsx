fd3777ba34b4f984b0fcbc9e17ac40b33d879efe
sethuragavan-git <727823tuit214@skct.edu.in>
2025-09-13 15:01:28 +0530

    Update QuizePage.jsx

--- reactapp/src/components/QuizePage.jsx
+++ reactapp/src/components/QuizePage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import { coursesAPI } from './api'; 
-import './QuizePage.css';//
+import { coursesAPI } from './api'; // Import your API functions
+import './QuizePage.css';
 
 const QuizPage = () => {
   const { courseId } = useParams();
